51d5a59cc836df481f298158684fd36ef95a0c29
r2r-company <git@r2r-company.com.ua>
2025-06-20 18:01:00 +0300

    initial commit

--- NashCRM/settings.py
+++ NashCRM/settings.py
@@ -73,7 +73,6 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -307,11 +306,23 @@ UNFOLD = {
         },
     ],
 }
+
 CORS_ALLOWED_ORIGINS = [
-    "*"
+    "http://localhost:3000",  # Ваш фронтенд
+    "http://127.0.0.1:3000",
 ]
-
-CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
 
-
+# Додайте ці налаштування:
+CORS_ALLOWED_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
